0a5e8e5304a2f733321783a02f490e295eae318f
SauroMatheo <sauromatheo@gmail.com>
2022-12-01 10:05:16 +0100

    importation du sql, connection reussie

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -7,11 +7,37 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+
+    public static Connection connect() {
+        String dbURL = "jdbc:mysql://localhost:3306/";
+        String nomUtilisateur = "root";
+        String mdp = "";
+
+        try {
+            // Connexion en passant les informations spécifiées précédemment
+            Connection conn = DriverManager.getConnection(dbURL, nomUtilisateur, mdp);
+            // Si on est bien connecté à la DB
+            if (conn != null) {
+                // On affiche un petit message sur le terminal
+                System.out.println("Connexion réussie !");
+
+                return conn;
+            }
+        } catch (SQLException ex) {
+            // Code de traitement d'erreur
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
 
     private static Scene scene;
 
@@ -32,6 +58,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+        connect();
         launch();
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module fr.dampierre {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens fr.dampierre to javafx.fxml;
+
     exports fr.dampierre;
 }
